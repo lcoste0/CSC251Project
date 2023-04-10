2aed0d825ac99a806e258471e27bdb9a85c81a6e
lcoste0 <lilianacoste04@gmail.com>
2023-04-10 09:01:43 -0400

    Update Policy.java
    Added documentation comments.

--- Policy.java
+++ Policy.java
@@ -22,8 +22,10 @@ public class Policy
     policyHolderHeight = 0.0;
     policyHolderWeight = 0.0;
   }
-  //constructor that accepts arguments
-  //parameters num, provider, firstName, lastName, age, smokeStat, height, weight to store info
+  /**
+  constructor that accepts arguments
+  @param num, provider, firstName, lastName, age, smokeStat, height, weight to store info
+  */
   public Policy(int num, String provider, String first, String last, int age, String smokeStat, double height, double weight)
   {
     policyNum = num;
@@ -37,90 +39,140 @@ public class Policy
   }
   //setter methods
   /**
-   set policy num will set the policy number
+   setPolicyNum will set the policy number
+   @param num The policy number
   */
   public void setPolicynum(int num)
   {
    policyNum = num;
   }
-  
+  /**
+   setProviderName will set the provider's name
+   @param provider The provider's name
+  */
   public void setProviderName(String provider)
   {
    providerName = provider;
   }
-  
+  /**
+   setFirstName will set the first name of the policy holder
+   @param first The policy holder's first name
+  */
   public void setFirstName(String first)
   {
    policyHolderFirstName = first;
   }
-  
+  /**
+   setLastName will set the last name of the policy holder
+   @param last The policy holder's last name
+  */
   public void setLastName(String last)
   {
    policyHolderLastName = last;
   }
-  
+  /**
+   setAge will set the age of the policy holder
+   @param age The policy holder's age
+  */
   public void setAge(int age)
   {
    policyHolderAge = age;
   }
-  
+  /**
+   setSmokingStatus will set the smoking status of the policy holder
+   @param smokeStat The policy holder's smoking status
+  */
   public void setSmokingStatus(String smokeStat)
   {
    policyHolderSmokingStat = smokeStat;
   }
-  
+  /**
+   setHeight will set the height of the policy holder
+   @param height The policy holder's height
+  */
   public void setHeight(double height)
   {
    policyHolderHeight = height;
   }
-  
+  /**
+   setWeight will set the weight of the policy holder
+   @param weight The policy holder's weight
+  */
   public void setWeight(double weight)
   {
    policyHolderWeight = weight;
   }
   
   //getter methods
+  /**
+   getPolicyNum method
+   @return The policy number
+  */
   public int getPolicyNum()
   {
    return policyNum;
   }
-  
+  /**
+   getProviderName method
+   @return The provider's name
+  */
   public String getProviderName()
   {
    return providerName;
   }
-  
+  /**
+   getFirstName method
+   @return The policy holder's first name
+  */
   public String getFirstName()
   {
    return policyHolderFirstName;
   }
-  
+  /**
+   getLastName method
+   @return The policy holder's last name
+  */
   public String getLastName()
   {
    return policyHolderLastName;
   }
-  
+  /**
+   getAge method
+   @return The policy holder's age
+  */
   public int getAge()
   {
    return policyHolderAge;
   }
-  
+  /**
+   getSmokingStatus method
+   @return The policy holder's smoking status
+  */
   public String getSmokingStatus()
   {
    return policyHolderSmokingStat;
   }
-  
+  /**
+   getHeight method
+   @return The policy holder's height
+  */
   public double getHeight()
   {
    return policyHolderHeight;
   }
-  
+  /**
+   getWeight method
+   @return The policy holder's weight
+  */
   public double getWeight()
   {
    return policyHolderWeight;
   }
 
-  //calculateBMI
+  /**
+   calculateBMI method
+   @return The policy holder's BMI
+  */
   public double calculateBMI()
   {
    final int BMI_NUM = 703;
@@ -128,6 +180,10 @@ public class Policy
   }
   
   //calculate and return the price of insurance policy
+  /**
+   calculateinsurancePolicy method
+   @return The calculation of the policy holder's insurance policy
+  */
   public double calculateInsurancePolicy()
   {
    final int BASE_FEE = 600;
@@ -137,7 +193,6 @@ public class Policy
    {
     additionalFee += 75;
    }
-   
  
    //determine if Policy Holder is a smoker
    if(policyHolderSmokingStat.equalsIgnoreCase("smoker"))
@@ -154,7 +209,10 @@ public class Policy
    return BASE_FEE + additionalFee;
  
   }
-  
+  /**
+   displayInformation method
+   display the policy holder's information
+  */
   public void displayInformation()
   {
    System.out.println("Policy Number: " + getPolicyNum());
